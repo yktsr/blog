f4e5fe14eaa7e83bec51cf033670bc9001bc6e11
yktsr <katsura.y.ac@icloud.com>
2017-05-19 15:21:13 +0900

    first commit

---
+++ content/test.md
@@ -0,0 +1,14 @@
+Title: title
+Date: 2017-05-19 14:25
+Modified: 2017-05-19 14:25
+Tags: tag1,tag2
+Category: cat1
+Authors: yktsr
+Summary: daitai
+
+# We are Evermore!
+
+## test sample 
+
+
+  markdown
---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'yktsr'
+SITENAME = '電気羊の夢をみる'
+SITEURL = 'https://yktsr.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'jp'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME = 'themes/pelican-alchemy/alchemy/'
+SITESUBTITLE = 'evermore!'
+DESCRIPTION = 'Security AI Artificial Intelligence'
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
+
+ICONS = (
+    ('github', 'https://github.com/nairobilug/pelican-alchemy'),
+    ('rss', 'https://github.com/'),
+)
